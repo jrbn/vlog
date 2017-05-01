9720a3251459f33d5d3943d627bcf099dbb1493f
unmeshvrije <unmesh.joshi126@gmail.com>
2017-05-01 14:44:11 +0200

    added script to generate and run queries on dbpedia

---
+++ scripts/query-dbpedia.sh
@@ -0,0 +1,1 @@
+python generate-queries.py --rules ~/vlog/examples/rules/aaai2016/DBpedia_L.dlog --mat /var/scratch/uji300/vlog/materialization_dbpedia --conf edb-dbpedia.conf --out dbpedia-queries.out
