22d0c2f8843b002f6f33384894fe183f3e06f35a
unmeshvrije <unmesh.joshi126@gmail.com>
2018-01-17 17:49:18 +0100

    relative accuracy calculation

--- scripts/IJCAI-results/thresholds.py
+++ scripts/IJCAI-results/thresholds.py
@@ -2,7 +2,7 @@ import argparse
 import copy
 import os
 from subprocess import check_output, STDOUT
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
@@ -26,6 +26,8 @@ def perf_measures(yActual, yPredicted):
     FP = 0
     FN = 0
     TN = 0
+    magicGuess = 0
+    qsqrGuess = 0
 
     if (len(yActual) != len(yPredicted)):
         print("FATAL", len(yActual) , " ! = " , len(yPredicted))
@@ -34,12 +36,16 @@ def perf_measures(yActual, yPredicted):
         if yActual[i] == yPredicted[i]:
             TP += 1
             TN += 1
+            if (yActual[i] == 0):
+                magicGuess += 1
+            else:
+                qsqrGuess += 1
         else:
             FN += 1
             FP += 1
-    return float(TP), float(FP), float(TN), float(FN)
+    return float(TP), float(FP), float(TN), float(FN), qsqrGuess, magicGuess
 
-def train_and_eval(train_file, test_file, columns, targetColumn):
+def train_and_eval(train_file, test_file, columns, targetColumn, estimator):
     # Use different files for training and test features
     X_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True, usecols=columns, engine="python")
     Y_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True, usecols=[targetColumn], engine="python")
@@ -49,9 +55,8 @@ def train_and_eval(train_file, test_file, columns, targetColumn):
     Y_train = np.ravel(Y_train)
     Y_test = np.ravel(Y_test)
 
-    std_clf = make_pipeline(MinMaxScaler(), LogisticRegression())
-    #std_clf = make_pipeline(StandardScaler(), LogisticRegression())
-    #std_clf = make_pipeline(RobustScaler(), LogisticRegression())
+    #std_clf = make_pipeline(estimator)
+    std_clf = make_pipeline(MinMaxScaler(), estimator)
     std_clf.fit(X_train, Y_train)
 
     startTime = datetime.now()
@@ -59,7 +64,20 @@ def train_and_eval(train_file, test_file, columns, targetColumn):
     endTime = datetime.now()
     print ("time to predict : ", endTime-startTime)
     #print ("Cross validation score : ", cross_val_score(LogisticRegression(), X, Y).mean())
-    return pred_test, metrics.accuracy_score(Y_test, pred_test)
+    #return pred_test, metrics.accuracy_score(Y_test, pred_test)
+    TP, FP, TN, FN, qsqrGuess, magicGuess = perf_measures(list(Y_test), list(pred_test))
+    nQsqrQueries = list(Y_test).count(1)
+    nMagicQueries = list(Y_test).count(0)
+    precision = float(TP) / float(TP + FP)
+    recall = float(TP) / float(TP + FN)
+    #f1score = 2*precision * recall / (precision + recall)
+    f1score = precision
+    print("Overall Accuracy = ", f1score)
+    if (nQsqrQueries != 0):
+        print("Relative Accuracy (QSQR) = ", (float(qsqrGuess)/float(nQsqrQueries))*100)
+    if (nMagicQueries != 0):
+        print("Relative Accuracy (MAGIC) = ", (float(magicGuess)/float(nMagicQueries))*100)
+    return pred_test, f1score
 
 def checkAccuracy(fileName, column, threshold):
     yPredicted = []
@@ -67,46 +85,37 @@ def checkAccuracy(fileName, column, threshold):
     with open(fileName, 'r') as fin:
         for line in fin.readlines():
             columns = line.split(',')
-            if (float(columns[column]) > threshold):
+            if (float(columns[column]) < threshold):
                 yPredicted.append(0)
             else:
                 yPredicted.append(1)
             # Last column is always the result column
             yExpected.append(float(columns[-1]))
 
-    TP, FP, TN, FN = perf_measures(list(yExpected), list(yPredicted))
+    TP, FP, TN, FN, qsqrGuess, magicGuess = perf_measures(list(yExpected), list(yPredicted))
+    nQsqrQueries = yExpected.count(1)
+    nMagicQueries = yExpected.count(0)
     precision = TP / (TP + FP)
     recall = TP / (TP + FN)
     f1score = 2*precision * recall / (precision + recall)
     #print (metrics.accuracy_score(yTest, predicted))
     #print ("Precision = ", precision)
     #print("Recall = ", recall)
-    print("Accuracy = ", f1score, " for feature ", COLUMNS[column])
+    print("Accuracy on Test set = ", f1score*100)
+    print("Relative Accuracy (QSQR) = ", (float(qsqrGuess)/float(nQsqrQueries))*100)
+    print("Relative Accuracy (MAGIC) = ", (float(magicGuess)/float(nMagicQueries))*100)
     return f1score
 
 def findThreshold(fileName, column):
-    xTemp = []
+    X = []
     Y = []
     with open(fileName, 'r') as fin:
         for line in fin.readlines():
-            # split into columns and calculate mean, min, max
             columns = line.split(',')
-            xTemp.append(float(columns[column]))
+            X.append(float(columns[column]))
             # Last column is always the result column
             Y.append(float(columns[-1]))
-    X = np.array(xTemp, dtype=np.float64)
-    avgX = np.mean(X)
-    minX = np.min(X)
-    maxX = np.max(X)
 
-    #if (column == 0):
-        # Min-Max Normalization
-    #    for i,x in enumerate(X):
-    #        X[i] = float(x - avgX) / float(maxX - minX)
-
-    #minX = np.min(X)
-    #maxX = np.max(X)
-    #print("Feature : ", COLUMNS[column], " => Min = ", minX , " Max = ", maxX)
     N = len(X)
     if (N != len(Y)):
         print("X and Y arrays length differ")
@@ -118,13 +127,11 @@ def findThreshold(fileName, column):
     for i in  sortedX:
         hits = 0
         misses = 0
-        #if (i % 10000 == 0):
-        #    print (i, ":")
         for x,y in zip(X,Y):
             # y value = 0 is for magic sets
-            if (x > i and y == 0):
+            if (x < i and y == 0):
                 hits += 1
-            elif (x < i and y == 1):
+            elif (x >= i and y == 1):
                 hits += 1
             else:
                 misses += 1
@@ -149,15 +156,15 @@ columns = []
 for i in range(0,nFeatures):
     columns.append(i)
 
-predictions, accuracy = train_and_eval(train, test, columns, nFeatures)
-print ("Overall accuracy = ", accuracy)
+predictions, accuracy = train_and_eval(train, test, columns, nFeatures, LogisticRegression())
+#print ("Overall accuracy = ", accuracy)
+
+#train_and_eval(train, test, [0], nFeatures, LinearRegression())
 
 print ("Individual feature wise accuracy: ")
 for i in range(6):
     thr, acc = findThreshold(train, i)
-    print("Feature : ", COLUMNS[i], " => ", thr , " (", acc , ")" )
+    print("===================================================================")
+    print("Feature : ", COLUMNS[i], " =>  threshold = ", thr , " (Accuracy on Training set = ", acc , ")" )
     checkAccuracy(test, i, thr)
-
-
-#test_normalized = normalizeColumn(test, 0)
-
+print("===================================================================")
